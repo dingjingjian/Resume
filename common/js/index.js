e09dd63367c9efe11861dd47b992dbd4b6db78b1
dingjingjian <dingjingjian@qq.com>
2019-02-13 16:04:59 +0800

    2019.02.13

--- common/js/index.js
+++ common/js/index.js
@@ -1,40 +1,29 @@
- //下拉元素
- layui.use('element', function(){
+layui.use(['element', 'code', 'layer','util'], function () {
+    //基本元素
     var element = layui.element;
-  });
-
-//弹窗
-
-layui.use('layer', function(){ //独立版的layer无需执行这一句
-  var $ = layui.jquery, layer = layui.layer; //独立版的layer无需执行这一句
-  
-  $('#about').bind('click', function(){
-    layer.open({
-        type: 1
-        ,title: false //不显示标题栏
-        ,closeBtn: false
-        ,area: '300px;'
-        ,shade: 0.8
-        ,btn: ['知道啦']
-        ,moveType: 1 //拖拽模式，0或者1
-        ,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;"><p class="me-h2 me-p">丁璟剑</p><p class="me-p">电话：13545171767</p><p class="me-p">邮箱：dingjingjian@qq.com</p></div>'
-        ,success: function(layero){
-          var btn = layero.find('.layui-layer-btn');
-          btn.css('text-align', 'center');
-          btn.find('.layui-layer-btn0').attr({
-            target: '_blank'
-          });
-        }
-      });
+    //代码修饰器
+    layui.code({
+        encode:true,
+        about:false
     });
-  
-});
-
-//固定栏
-layui.use('util', function () {
+    //弹窗
+    var layer = layui.layer;
+    $('#about').on('click', function () {
+        layer.open({
+            type: 1
+            , title: false
+            , closeBtn: false
+            , area: '300px;'
+            , shade: 0.8
+            , btn: ['知道啦']
+            , btnAlign: 'c'
+            , content: '<div style="padding: 50px 20px 40px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;"><p class="me-h2 me-p">丁璟剑</p><p class="me-p">电话：13545171767</p><p class="me-p">邮箱：dingjingjian@qq.com</p><img style="width: 145px;margin: 20px auto 0;display: block;" src="common/images/me.jpg"></div>'
+            , success: function (layero) {
+            }
+        });
+    });
+    //固定工具栏
     var util = layui.util;
-
-    //执行
     util.fixbar({
         bar1: true
         , click: function (type) {
@@ -44,11 +33,11 @@ layui.use('util', function () {
                     isIphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
                     isAndroid = ua.match(/(Android)\s+([\d.]+)/),
                     isMobile = isIphone || isAndroid;
-                    if(isMobile) {
-                        window.location.href="mqqwpa://im/chat?chat_type=wpa&uin=464759011&version=1&src_type=web&web_src=oicqzone.com"
-                    }else{
-                        window.location.href="tencent://message/?uin=464759011&Site=http://vps.shuidazhe.com&Menu=yes"
-                    }
+                if (isMobile) {
+                    window.location.href = "mqqwpa://im/chat?chat_type=wpa&uin=464759011&version=1&src_type=web&web_src=oicqzone.com"
+                } else {
+                    window.location.href = "tencent://message/?uin=464759011&Site=http://vps.shuidazhe.com&Menu=yes"
+                }
             }
         }
     });
